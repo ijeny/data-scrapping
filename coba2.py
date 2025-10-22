6c97d271f6f78c195f7c3f3e7f818b01a6e6f2a8
ijeny <imtinan.jmb6628@gmail.com>
2025-10-22 17:50:25 +0700

    Add files via upload

---
+++ coba2.py
@@ -0,0 +1,23 @@
+import requests
+import scraping
+from bs4 import BeautifulSoup
+
+# fungsi = requests.get("https://www.detik.com/")
+
+# print(result)
+# print(result.encoding)
+# print(result.status_code)
+# print(result.elapsed)
+# print(result.url)
+# print(result.history)
+# print(result.headers['Content-Type'])
+
+def main_scraper(url,directory):
+    scraping.create_directory(directory) 
+    source_code = requests.get(url) 
+    source_text = source_code.text 
+    soup = BeautifulSoup(source_text,"html.parser")
+    print(soup.find_all('div',{'class':['box cb cb-video','grid-row list-content list-content--column', 'media__title']}))
+
+main_scraper("https://www.detik.com/", "hasil")
+# print(scraping)
